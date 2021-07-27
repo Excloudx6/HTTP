015beae60b98a3ac8e62cb4eec057594d78f9e9c
Jared <jaredturck9@gmail.com>
2021-07-27 02:07:17 +0100

    Optimised get_data
    get_data is a lot faster now, moved everything outside of the loop, created separate thread for printing the upload percentage.

--- webserver_3.py
+++ webserver_3.py
@@ -1,4 +1,4 @@
-import socket, re, time, os
+import socket, re, time, os, threading
 
 class Server():
     def __init__(self):
@@ -6,34 +6,53 @@ class Server():
         self.host_port = 81
         self.s = socket.socket()
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.data_recv_size = 1024
+        self.data_recv_size = 16384
         self.ok_200 = b"HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n"
         self.max_size = 100_000_000
 
-    def get_data(self, conn, size=-1):
-        data = b""
-        byte_count = 0
-        prev_time = time.time() + 1
-        while True:
-            chunk = conn.recv(self.data_recv_size)
-            byte_count += self.data_recv_size
-
-            if time.time() >= prev_time:
-                prev_time = time.time() + 1
-                percent = ((byte_count / int(size))*100)
+    def print_percent(self, size):
+        ''' Purely for visual purposes, shows a percent complete while downloading the file'''
+        if size > -1:
+            print("[+] Downloading file!")
+            start = time.time()
+            self.byte_count = len(self.data)
+            percent = ((self.byte_count / int(size))*100)
+            while percent < 100:
+                self.byte_count = len(self.data)
+                percent = ((self.byte_count / int(size))*100)
                 print(f"[+] Downloading file {percent}% complete!")
+                time.sleep(2)
+            print(f"[+] Total download time {round(time.time() - start, 2)} seconds")
+
+    def get_data(self, conn, size=-1, do_print=True):
+        ''' Fetches the file from client, call this method when respoding to POST'''
+        self.data = b""
+        self.byte_count = 0
+        if do_print == True:
+            show_percent = threading.Thread(target=self.print_percent, args=[size])
+            show_percent.start()
+
+        # process packet header (loop until recived data is less then 16384)
+        if size == -1:
+            chunk = conn.recv(self.data_recv_size)
+            while len(chunk) >= self.data_recv_size:
+                chunk = conn.recv(self.data_recv_size)
+                self.data += chunk
                 
-            if size == -1 and len(chunk) < self.data_recv_size:
-                return data + chunk
+            return self.data + chunk
+
+        # process packet body (loop for length of body)
+        # to make this loop as fast as possible, it should only contain conn.recv
+        for i in range(0, size, self.data_recv_size):
+            self.data += conn.recv(self.data_recv_size)
             
-            elif byte_count >= size:
-                return data + chunk
-            else:
-                data += chunk
+        return self.data
                 
     def save_file(self, body):
+        print("[+] extracting file data!")
         name = re.compile(b'name="uploadedfile"; filename="(.+)"').search(body).group(1)
         data = re.compile(b"WebKitFormBoundary((\n|.)*)Content-Type.+\n.+?\n((\n|.)*)([\-]+WebKitFormBoundary)?")
+        print("[+] Saving file to disk!")
         with open(name, "wb") as file:
             file.write(data.search(body).group(3))
         os.popen("python zcreate_index_post.py")
@@ -58,6 +77,7 @@ class Server():
                 # POST request
                 elif header[0:4] == b"POST":
                     length = int(header.split(b"Content-Length: ")[1].split(b"\n")[0].decode("ASCII"))
+                    print([length])
 
                     # check for large file
                     if length >= self.max_size:
@@ -88,3 +108,16 @@ class Server():
 
 s = Server()
 s.run()
+
+# to do list:
+# inside of get_data
+# packet processing loop could use multi-threading to download multiple chunks at once
+# this may make it faster
+
+# ram:
+# memory managment needs to be inplace for very large files
+# files too large to fit completly in RAM.
+
+# re:
+# the save file regular expression is taking way to long
+# don't use expressions in save_file
